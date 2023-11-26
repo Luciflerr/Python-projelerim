ecdb25885dd202436a8ef0a1cf295998796c5216
Eray <129649763+Luciflerr@users.noreply.github.com>
2023-11-26 19:25:55 +0300

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+nereye = input("Nereye gittin?")
+kimi = input("Kimi gördün?")
+yaptı = input("Ne yaptı?")
+duygu = input("Ne hissettin?")
+
+print(f"Bugün {nereye} gittim ve")
+print(f"{kimi}gördüm.")
+print(f"sonra bir anda {yaptı}.")
+print(f"o an {duygu} hissettim.")
