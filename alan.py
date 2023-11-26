e2e7d07533fa63bd5377ca2d37af910afb1fc411
Eray <129649763+Luciflerr@users.noreply.github.com>
2023-11-26 19:45:18 +0300

    Add files via upload

---
+++ alan.py
@@ -0,0 +1,7 @@
+# Bu bir alan hesaplayıcıdır.
+
+uzunluk = float(input(f"Lütfen uzunluğu giriniz."))
+genislik = float(input(f"Lütfen genişliği giriniz."))
+
+alan = uzunluk * genislik
+print(f"Alan tam olarak {alan}cm^2'dir.")
