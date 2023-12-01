1cb5a74e58933990b583f9fa756a664260ec3cab
Eray <129649763+Luciflerr@users.noreply.github.com>
2023-12-01 17:34:59 +0300

    Update and rename Pratice4.py to 1-100 arası asal sayı.py

--- 1-100 arası asal sayı.py
+++ 1-100 arası asal sayı.py
@@ -1,7 +1,7 @@
-# Ekranda asal sayı olup olmadığını kontrol eden bir program yazın.
+# Ekranda 100'e kadar asal sayı olup olmadığını kontrol eden bir program yazın.
 asal = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
-soru = int(input("Lütfen sayı giriniz."))
+soru = int(input("Lütfen sayınızın asal olup olmadığını öğrenmek için 1 ile 100 arası sayınızı giriniz."))
 print(soru)
 
 for i in asal:
@@ -11,4 +11,3 @@ for i in asal:
     if not i == soru:
         print("Bu sayı asal sayı değildir.")
         break
-
